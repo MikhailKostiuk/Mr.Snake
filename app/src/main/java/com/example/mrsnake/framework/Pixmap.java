35cbd2a3ea101fe8579307a4389a4abb0bdec52a
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2017-02-17 13:06:31 +0200

    Add Graphics and Pixmap interfaces to the framework.

--- app/src/main/java/com/example/mrsnake/framework/Pixmap.java
+++ app/src/main/java/com/example/mrsnake/framework/Pixmap.java
@@ -2,4 +2,12 @@ package com.example.mrsnake.framework;
 
 
 public interface Pixmap {
+
+    int getWidth();
+
+    int getHeight();
+
+    Graphics.PixmapFormat getFormat();
+
+    void dispose();
 }
