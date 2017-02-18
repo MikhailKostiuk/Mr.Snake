7bb9046e958582e0f9a233aa6dd4a6b3026a4f7e
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2017-02-18 20:13:55 +0200

    Add AndroidFastRenderView for doing all rendering via SurfaceView.

---
+++ app/src/main/java/com/example/mrsnake/framework/impl/AndroidFastRenderView.java
@@ -0,0 +1,59 @@
+package com.example.mrsnake.framework.impl;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+
+public class AndroidFastRenderView extends SurfaceView implements Runnable {
+
+    AndroidGame mGame;
+    Bitmap mFrameBuffer;
+    Thread mRenderThread = null;
+    SurfaceHolder mSurfaceHolder;
+    volatile boolean running = false;
+
+    public AndroidFastRenderView(AndroidGame game, Bitmap frameBuffer) {
+        super(game);
+        this.mGame = game;
+        this.mFrameBuffer = frameBuffer;
+        this.mSurfaceHolder = getHolder();
+    }
+
+    public void pause() {
+        running = false;
+        while(true) {
+            try {
+                mRenderThread.join();
+                return;
+            } catch (InterruptedException e) {
+                // retry
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        Rect dstRect = new Rect();
+        long startTime = System.nanoTime();
+
+        while (running) {
+            if (!mSurfaceHolder.getSurface().isValid()) {
+                continue;
+            }
+
+            float deltaTime = (System.nanoTime() - startTime) / 1_000_000_000.0f;
+            startTime = System.nanoTime();
+
+            mGame.getCurrentScreen.update(deltaTime);
+            mGame.getCurrentScreen.present(deltaTime);
+
+            Canvas canvas = mSurfaceHolder.lockCanvas();
+            canvas.getClipBounds(dstRect);
+            canvas.drawBitmap(mFrameBuffer, null, dstRect, null);
+            mSurfaceHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+}
