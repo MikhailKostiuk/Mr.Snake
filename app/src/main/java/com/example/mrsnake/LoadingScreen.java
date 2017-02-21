3d6fdadf45397510e0e922f9d509c3cebc2e0559
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2017-02-21 14:25:20 +0200

    Add MainMenuScreen and LoadingScreen.

---
+++ app/src/main/java/com/example/mrsnake/LoadingScreen.java
@@ -0,0 +1,68 @@
+package com.example.mrsnake;
+
+import com.example.mrsnake.framework.Game;
+import com.example.mrsnake.framework.Graphics;
+import com.example.mrsnake.framework.Screen;
+import com.example.mrsnake.framework.Graphics.PixmapFormat;
+
+
+public class LoadingScreen extends Screen {
+
+    public LoadingScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+        Graphics graphics = mGame.getGraphics();
+
+        Assets.background = graphics.newPixmap("background.png", PixmapFormat.RGB565);
+        Assets.logo = graphics.newPixmap("logo.png", PixmapFormat.ARGB4444);
+        Assets.mainMenu = graphics.newPixmap("mainmenu.png", PixmapFormat.ARGB4444);
+        Assets.buttons = graphics.newPixmap("buttons.png", PixmapFormat.ARGB4444);
+        Assets.help1 = graphics.newPixmap("help1.png", PixmapFormat.ARGB4444);
+        Assets.help2 = graphics.newPixmap("help2.png", PixmapFormat.ARGB4444);
+        Assets.help3 = graphics.newPixmap("help3.png", PixmapFormat.ARGB4444);
+        Assets.numbers = graphics.newPixmap("numbers.png", PixmapFormat.ARGB4444);
+        Assets.ready = graphics.newPixmap("ready.png", PixmapFormat.ARGB4444);
+        Assets.pause = graphics.newPixmap("pausemenu.png", PixmapFormat.ARGB4444);
+        Assets.gameOver = graphics.newPixmap("gameover.png", PixmapFormat.ARGB4444);
+        Assets.headUp = graphics.newPixmap("headup.png", PixmapFormat.ARGB4444);
+        Assets.headLeft = graphics.newPixmap("headleft.png", PixmapFormat.ARGB4444);
+        Assets.headDown = graphics.newPixmap("headdown.png", PixmapFormat.ARGB4444);
+        Assets.headRight = graphics.newPixmap("headright.png", PixmapFormat.ARGB4444);
+        Assets.tail = graphics.newPixmap("tail.png", PixmapFormat.ARGB4444);
+        Assets.apple = graphics.newPixmap("stain1.png", PixmapFormat.ARGB4444);
+        Assets.pineapple = graphics.newPixmap("stain2.png", PixmapFormat.ARGB4444);
+        Assets.grapes = graphics.newPixmap("stain3.png", PixmapFormat.ARGB4444);
+
+        Assets.click = mGame.getAudio().newSound("click.ogg");
+        Assets.eat = mGame.getAudio().newSound("eat.ogg");
+        Assets.bitten = mGame.getAudio().newSound("bitten.ogg");
+
+        Settings.load(mGame.getFileIO());
+
+        mGame.setScreen(new MainMenuScreen(mGame));
+    }
+
+    @Override
+    public void present(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ app/src/main/java/com/example/mrsnake/MainMenuScreen.java
@@ -0,0 +1,100 @@
+package com.example.mrsnake;
+
+import com.example.mrsnake.framework.Game;
+import com.example.mrsnake.framework.Graphics;
+import com.example.mrsnake.framework.Input;
+import com.example.mrsnake.framework.Screen;
+
+import java.util.List;
+
+
+public class MainMenuScreen extends Screen {
+
+    public MainMenuScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+        Graphics graphics = mGame.getGraphics();
+        List<Input.TouchEvent> touchEvents = mGame.getInput().getTouchEvents();
+        mGame.getInput().getKeyEvents();
+
+        int length = touchEvents.size();
+        for (int i = 0; i < length; i++) {
+            Input.TouchEvent touchEvent = touchEvents.get(i);
+            if (touchEvent.type == Input.TouchEvent.TOUCH_UP) {
+
+                if (inBounds(touchEvent, 0, graphics.getHeight() - 300, 300, 300)) {
+                    Settings.sSoundEnabled = !Settings.sSoundEnabled;
+                    if (Settings.sSoundEnabled) {
+                        Assets.click.play(1);
+                    }
+                }
+
+                if (inBounds(touchEvent, 340, 760, 400, 200)) {
+                    mGame.setScreen(new GameScreen(mGame));
+                    if (Settings.sSoundEnabled) {
+                        Assets.click.play(1);
+                    }
+                    return;
+                }
+
+                if (inBounds(touchEvent, 20, 999, 1040, 200)) {
+                    mGame.setScreen(new HighScoresScreen(mGame));
+                    if (Settings.sSoundEnabled) {
+                        Assets.click.play(1);
+                    }
+                    return;
+                }
+
+                if (inBounds(touchEvent, 340, 1039, 400, 200)) {
+                    mGame.setScreen(new HelpScreen(mGame));
+                    if (Settings.sSoundEnabled) {
+                        Assets.click.play(1);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void present(float deltaTime) {
+        Graphics graphics = mGame.getGraphics();
+
+        graphics.drawPixmap(Assets.background, 0, 0);
+        graphics.drawPixmap(Assets.logo, 106, 105);
+        graphics.drawPixmap(Assets.mainMenu, 20, 760);
+        if (Settings.sSoundEnabled) {
+            graphics.drawPixmap(Assets.buttons, 0, 1620, 0, 0, 300, 300);
+        } else {
+            graphics.drawPixmap(Assets.buttons, 0, 1620, 300, 0, 300, 300);
+        }
+    }
+
+    @Override
+    public void pause() {
+        Settings.save(mGame.getFileIO());
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    private boolean inBounds(Input.TouchEvent touchEvent, int x, int y, int width, int height) {
+        if (touchEvent.x > x && touchEvent.x < x + width - 1
+                && touchEvent.y > y && touchEvent.y < y + height - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
